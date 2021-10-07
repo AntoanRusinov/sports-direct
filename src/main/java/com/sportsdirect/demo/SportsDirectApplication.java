d032f871ded00daf942ef1f557a0dc077bef3c61
AntoanRusinov <antoan.rusinov@gmail.com>
2021-10-07 11:30:42 +0300

    Initial commit for project skeleton

---
+++ src/main/java/com/sportsdirect/demo/SportsDirectApplication.java
@@ -0,0 +1,13 @@
+package com.sportsdirect.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsDirectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsDirectApplication.class, args);
+	}
+
+}
